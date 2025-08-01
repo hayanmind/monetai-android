7d15a2b277af726b9a440375a66ed3acf5052779
GitHub Action <action@github.com>
2025-08-01 04:18:59 +0000

    chore: bump version to 1.0.0-beta.2

--- monetai-sdk/src/main/java/com/monetai/sdk/SDKVersion.kt
+++ monetai-sdk/src/main/java/com/monetai/sdk/SDKVersion.kt
@@ -4,7 +4,7 @@ package com.monetai.sdk
  * SDK version information
  */
 object SDKVersion {
-    private const val VERSION = "1.0.0-beta.1"
+    private const val VERSION = "1.0.0-beta.2"
     
     @JvmStatic
     fun getVersion(): String = VERSION
